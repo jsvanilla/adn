321940b57ed1653b00e273a7dab7dffdd382b4ac
Miguel Segura <mianse@hotmail.com>
2020-05-10 13:21:07 -0500

    Real solution

--- test/mutation/network_mutation.test.js
+++ test/mutation/network_mutation.test.js
@@ -1,20 +1,20 @@
-const supertest = require('supertest')
-// const app = require('../../api/index')
-// const express = require('express')
 const dataForTest = require('../dataForTest')
-
 const testServer = require('../testServer')
-const request = supertest.agent(testServer)
-// controller_mutation.test.js
-//const request = testServer('/')
+
 describe("primer test",() =>{
-  it("", done => {
-    request.post('/mutation',dataForTest.mutationTrue).expect(200).then(response => {
-      expect(response.status).toEqual(200)
+  it("mutation true", done => {
+    testServer().post('/mutation',dataForTest.mutationTrue).expect(200).then(response => {
+      expect(response.statusCode).toBe(200)
       // console.log(response.status)
       //expect(response.body).toHaveProperty('message', 'cualquier mensaje')
       // { boolean: true, text: '¡ Se encontró una mutación en la cadena de ADN !', status: 200 }}
+      done()
     })
-    done()
+  })
+  it("mutation false", done => {
+    testServer().post('/mutation',dataForTest.mutationFalse).then(response => {
+      expect(response.statusCode).toBe(500)
+      done()
+    })
   })
 })
--- test/testServer.js
+++ test/testServer.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const supertest = require('supertest')
+const swaggerUi = require('swagger-ui-express')
+const geneticMutation = require('../api/components/mutation/network_mutation')
+const swaggerDoc = require('../api/swagger.json')
 
-function testServer(route){
+function testServer(){
   const app = express()
-  // route(app)
+  app.use('/mutation', geneticMutation)
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
   return supertest(app)
 }
 
